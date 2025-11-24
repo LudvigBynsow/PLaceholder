60fccd405cf0ea2ac7c8b0c286df8113fd7f2609
Ludvig Bünsow <ludbns@edu.umea.se>
2025-11-24 14:15:04 +0100

    message

--- docs/_app/immutable/chunks/D1_qzhAy.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1noail4?.base??"",a=globalThis.__sveltekit_1noail4?.assets??s??"";export{a,s as b};
---
+++ docs/_app/immutable/chunks/DDK4J-5O.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1bp5vay?.base??"",a=globalThis.__sveltekit_1bp5vay?.assets??s??"";export{a,s as b};
